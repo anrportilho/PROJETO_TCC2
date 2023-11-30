529647016e4c60eb940f6be6e5952b06fb366ba4
Anderson N. R. Portilho <anderson.anr@gmail.com>
2023-11-30 13:14:39 -0300

    Atualização de usuario e senha

--- funcoes/conn_bd.py
+++ funcoes/conn_bd.py
@@ -3,8 +3,8 @@ import mysql.connector
 def connect():
     mydb = mysql.connector.connect(
     host="localhost",
-    user="root",
-    password="",
+    user="opera",
+    password="tgbhyt@iolAXr",
     database="projeto_tcc2")
     mydb.cursor()
     return mydb
