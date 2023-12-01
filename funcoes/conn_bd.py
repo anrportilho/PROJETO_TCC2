adcb4976791ff9814c5410c040c4103679a61aaa
Anderson N. R. Portilho <anderson.anr@gmail.com>
2023-11-30 23:15:36 -0300

    Atualização de usuario e senha Inclusão do painel web

--- funcoes/conn_bd.py
+++ funcoes/conn_bd.py
@@ -3,8 +3,8 @@ import mysql.connector
 def connect():
     mydb = mysql.connector.connect(
     host="localhost",
-    user="opera",
-    password="tgbhyt@iolAXr",
+    user="root",
+    password="",
     database="projeto_tcc2")
     mydb.cursor()
     return mydb
